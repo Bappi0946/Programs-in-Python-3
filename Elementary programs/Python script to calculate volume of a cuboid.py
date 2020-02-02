a77ec0dbc2089049606d44a89d7d82729767604b
Bappi Rahman <56030194+Bappi0946@users.noreply.github.com>
2020-02-02 20:34:28 +0600

    Add files via upload

---
+++ Elementary programs/Python script to calculate volume of a cuboid.py
@@ -0,0 +1,5 @@
+l = float(input('Enter length of a cuboid'))
+w = float(input('Enter width of a cuboid'))
+h = float(input('Enter yhe hight of a cuboid'))
+v = l * w * h
+print("The volume of the cuboid is:",v)
