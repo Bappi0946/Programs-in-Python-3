4c8b2160accfa0a38eab55db0aa0397c3c3ce8fb
Bappi Rahman <56030194+Bappi0946@users.noreply.github.com>
2020-02-02 19:00:20 +0600

    Add files via upload

---
+++ Elementary programs/Python script to calculate simple interest.py
@@ -0,0 +1,4 @@
+p = float(input('Enter the principal amount:'))
+r = float(input('Enter the rate of interest:'))
+n = float(input('Enter time in years:'))
+print("The amount of interest is:",p * r / 100 * n)
